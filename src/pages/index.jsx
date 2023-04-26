142f0b8515bf7e936eda1116de0551e4095870d8
ikrom97 <ikromr04@gmail.com>
2023-04-26 16:55:58 +0500

    favicon addition

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { AppRoute } from '@/const';
+import React from 'react';
 import PageWrapper from '@/components/layouts/page-wrapper/page-wrapper';
 import QuotesScreen from '@/components/screens/quotes-screen/quotes-screen';
 
 function Home() {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   router.push(AppRoute.Quotes)
-  // }, [router]);
-
   return (
     <PageWrapper withSidebar>
       <QuotesScreen />
